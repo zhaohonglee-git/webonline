6012d31caeb912e63675d33e0cf6223d1ba0c4b9
zhaohonglee <296222105@qq.com>
2020-12-15 18:00:47 +0800

    v1.0.1 second commit axios

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,51 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Rainbow from '../hoc/Rainbow'
+import axios from 'axios'
 
-const Home = () => {
-  return (
-    <div>
-      <div className='container'>
-        <h3 className='center'>Home页面</h3>
-        <p>欢迎来到webonline</p>
+
+
+class Home extends Component {
+
+  state = {
+    posts: []
+  }
+
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/posts').then(res => {
+      console.log(res)
+      this.setState({
+        posts: res.data.slice(0, 20)
+      })
+    })
+  }
+
+  render() {
+
+    const { posts } = this.state
+    const postList = posts.length > 0 ? (
+      posts.map(item => {
+        return (
+          <div className='post card' key={item.id}>
+            <div className='card-content'>
+              <span className='card-title'>{item.title}</span>
+              <p>{item.body}</p>
+            </div>
+          </div>
+        )
+      })
+    ) : (
+        <div className='center'>没有对应的博客文章进行展示</div>
+      )
+
+    return (
+      <div>
+        <div className='container'>
+          <h3 className='center'>Home页面</h3>
+          {postList}
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Rainbow(Home)
